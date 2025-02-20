d1f9e753f79ffc694f49daf5732b8c13fc06ee56
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-20 22:38:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active');  // Toggle the 'active' class
+    input.focus();  // Automatically focus on the input field
+});
